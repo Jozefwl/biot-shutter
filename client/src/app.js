3b34affb81f7216708b7ad8176f059fa1acbd1ba
Martin <martinsivak.15@gmail.com>
2024-06-01 00:04:46 +0200

    Routy

--- client/src/app.js
+++ client/src/app.js
@@ -1,44 +1,20 @@
 import './App.css';
+import React from 'react';
+import Register from './components/Register'
 import 'bootstrap/dist/css/bootstrap.min.css';
-import React from "react"
-import Name from "./components/Name"
-import Control from "./components/Control"
-import Settings from "./components/Settings"
-import { useDataContext } from './DataContext';
-import {Spinner, Card }from 'react-bootstrap';
-import toast, { Toaster } from 'react-hot-toast';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from "./routes/Home"
 
 function App() {
 
-  const { data, fetchData } = useDataContext()
-
   return (
     <div className="App">
-      <h1 className="Header">Blinds control</h1>
-      <div className="Card">
-      <Card>
-       {data ? (
-        <>
-          <Card.Header>
-            <Name name={data.blindStatus.name} id={data.blindStatus._id} fetchData={fetchData}/>
-          </Card.Header>
-          <Card.Body>
-          <Control id={data.blindStatus._id} pos={data.blindStatus.motorPosition} fetchData={fetchData}/>
-          <Settings 
-            id={data.blindStatus._id} 
-            useDaylight={data.blindStatus.daylightSensor} 
-            useTimeSettings={data.blindStatus.manualTimeSettings} 
-            events={data.scheduledEvents}
-            fetchData={fetchData}
-          />
-          </Card.Body>
-        </>
-       ):(
-        <Spinner animation="border" />
-       )}
-       </Card>
-       </div>
-      <Toaster />
+      <Router>
+        <Routes> 
+          <Route path="/" element={<Register/>}/>
+          <Route path="/home" Component={Home}/>
+        </Routes> 
+      </Router>
     </div>
   );
 }
--- client/src/config.js
+++ client/src/config.js
@@ -2,7 +2,8 @@ const config = {
     URI: 'https://waldhauser.sk:4000',
     blindsId: '66351b236ca9c24b6afd71e1',
     openPosition: 10000,
-    closePosition: 0
+    closePosition: 0,
+    REACT_APP_BACKEND_URI:'http://localhost:4000'
 
 }
 
